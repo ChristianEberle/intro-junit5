b1f89537f301d43e12b353bfb01c6b1758bce1e6
Christian Eberle <christian.eberle.78@gmx.de>
2025-11-18 09:00:20 +0100

    After unit 40. JUnit 5 Test Instance Lifecycle Demo

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,11 +1,15 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
 
     private Greeting greeting;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before - I am only called Once!!!");
+    }
 
     @BeforeEach
     void setUp() {
@@ -14,12 +18,27 @@ class GreetingTest {
     }
 
     @Test
-    void helloWorld() {
+    void testHelloWorld0() {
         System.out.println(greeting.helloWorld());
     }
 
     @Test
-    void testHelloWorld() {
+    void testHelloWorld1() {
         System.out.println(greeting.helloWorld("Christian"));
+    }
+
+    @Test
+    void testHelloWorld2() {
+        System.out.println(greeting.helloWorld("Sam"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In After Each...");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After - I am only called Once!!!");
     }
 }
